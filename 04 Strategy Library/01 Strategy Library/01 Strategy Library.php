0281d913d905ba2ec2e5f24710801ab4a3ad8429
Gustavo <g.aviles.segovia@gmail.com>
2018-05-24 17:52:49 -0400

    Create a strategy library php driven.

---
+++ 04 Strategy Library/01 Strategy Library/01 Strategy Library.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gustavo
+ * Date: 5/24/2018
+ * Time: 4:42 PM
+ */
+
+$strategyMap = [
+    [
+        'name' => 'Example 1',
+        'link' => 'strategy-library/example-1',
+        'description' => 'Testing description of testing tutorial 1'
+    ],
+    [
+        'name' => 'Example 2',
+        'link' => 'strategy-library/example-2',
+        'description' => 'Testing description of testing tutorial 2'
+    ],
+    [
+        'name' => 'Example 3',
+        'link' => 'strategy-library/example-3',
+        'description' => 'Testing description of testing tutorial 3'
+    ],
+];
+
+?>
+<div class="row">
+    <div class="search col-xs-12">
+        <input type="text" id="strategy-library-search-box" class="form-control input-lg" placeholder="Search Strategy Tutorials" onchange="Tutorials.OnSearchChange()" onkeyup="Tutorials.OnSearchChange()">
+    </div>
+</div>
+<br>
+<table class="table table-striped qc-table">
+    <thead>
+    <tr>
+        <th>
+            Strategy Name
+        </th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php foreach ($strategyMap as $strategy) { ?>
+        <tr>
+            <td>
+                <a class="docs-internal-link" href="/tutorials2/<?= $strategy['link'] ?>"><?= $strategy['name'] ?></a>
+                <p><?= $strategy['description'] ?></p>
+            </td>
+        </tr>
+    <?php } ?>
+    </tbody>
+</table>
+
+<style>
+    .documentation #doc-content .pre-title-area > span {
+        display: none;
+    }
+
+    .documentation #doc-content .pre-title-area > span:first-child {
+        display: inline;
+    }
+</style>
