58c87a2868a9c2ca1406c7eb0ccd35f02d63f9a1
Gustavo <g.aviles.segovia@gmail.com>
2018-05-24 18:49:45 -0400

    Self contain strategy library file

--- 04 Strategy Library/01 Strategy Library/01 Strategy Library.php
+++ 04 Strategy Library/01 Strategy Library/01 Strategy Library.php
@@ -27,7 +27,7 @@ $strategyMap = [
 ?>
 <div class="row">
     <div class="search col-xs-12">
-        <input type="text" id="strategy-library-search-box" class="form-control input-lg" placeholder="Search Strategy Tutorials" onchange="Tutorials.OnSearchChange()" onkeyup="Tutorials.OnSearchChange()">
+        <input type="text" id="strategy-library-search-box" class="form-control input-lg" placeholder="Search Strategy Tutorials" onchange="SLSearch.OnSearchChange()" onkeyup="SLSearch.OnSearchChange()">
     </div>
 </div>
 <br>
@@ -73,3 +73,79 @@ $strategyMap = [
         display: block;
     }
 </style>
+<script type="application/javascript">
+    /**
+     * SLSearch static controller
+     */
+    var SLSearch = /** @class */ (function () {
+        function SLSearch() {
+        }
+
+        /**
+         * Handler for the vent of change in the strategy library search box
+         * @constructor
+         */
+        SLSearch.OnSearchChange = function () {
+            if (!SLSearch._BuildSearchMap())
+                return;
+            var table = $('#strategy-library-table');
+            if (table.length == 0)
+                return false;
+            var search = $('#strategy-library-search-box').val();
+            var options = {
+                shouldSort: true,
+                threshold: 0.8,
+                location: 0,
+                distance: 100,
+                maxPatternLength: 32,
+                minMatchCharLength: 3,
+                keys: [
+                    "text"
+                ]
+            };
+            console.log('starting search');
+            var result = SLSearch._StrategyLibraryMap;
+            if (search != '') {
+                var fuse = new Fuse(SLSearch._StrategyLibraryMap, options);
+                result = fuse.search(search);
+                if (result.length == 0) {
+                    var cols = table.find('thead th').length;
+                    result = [{
+                        html: "<td colspan='" + cols + "' class='text-center' > No matching result </td>",
+                        text: ''
+                    }];
+                }
+            }
+            var tableHtml = '';
+            for (var i in result) {
+                tableHtml += '<tr>' + result[i].html + '</tr>';
+            }
+            table.find('tbody').html(tableHtml);
+            console.log(result);
+        };
+        /**
+         * Builds the strategy library search map
+         * @private
+         */
+        SLSearch._BuildSearchMap = function () {
+            if (SLSearch._StrategyLibraryMap !== null)
+                return true;
+            var table = $('#strategy-library-table');
+            if (table.length == 0)
+                return false;
+            SLSearch._StrategyLibraryMap = [];
+            table.find('tbody > tr').each(function (i, el) {
+                var text = $(el).text();
+                text = text.replace(/(\s+|\n)/g, ' ');
+                text = text.replace(/(\s+|\n)/g, ' ');
+                SLSearch._StrategyLibraryMap.push({
+                    'html': $(el).html(),
+                    'text': text
+                });
+            });
+            return true;
+        };
+        SLSearch._StrategyLibraryMap = null;
+        return SLSearch;
+    }());
+</script>
